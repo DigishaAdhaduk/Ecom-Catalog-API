48eef11e6d4912b85a5c6cccd39f321b085d9197
DigishaAdhaduk <digishaadhaduk@gmail.com>
2025-12-12 18:28:28 +0530

    Initial commit

---
+++ src/main/java/com/first/Ecom_Catalog_API/EcomCatalogApiApplication.java
@@ -0,0 +1,13 @@
+package com.first.Ecom_Catalog_API;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcomCatalogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcomCatalogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/first/Ecom_Catalog_API/EcomCatalogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.first.Ecom_Catalog_API;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcomCatalogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
