efac3af3e94cf527b144aad32e6e99adb09d52f5
DigishaAdhaduk <digishaadhaduk@gmail.com>
2026-01-16 10:20:37 +0530

    Fix import statements in ProductRepo.java

--- src/main/java/com/first/Ecom_Catalog_API/repo/ProductRepo.java
+++ src/main/java/com/first/Ecom_Catalog_API/repo/ProductRepo.java
@@ -1,9 +1,9 @@
-package com.first.Ecom_Catalog_API.repo;
+package com.first.Ecom_Catalog_API.repo
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
 
-import com.first.Ecom_Catalog_API.model.Product;
+import com.first.Ecom_Catalog_API.model.Product
 import org.springframework.data.jpa.repository.Query;
 import java.util.List;
 
